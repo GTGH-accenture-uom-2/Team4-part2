18d4407893e7f4061d0093042d8a04f0defce2c3
Matina <matinampoz@gmail.com>
2024-04-20 21:57:41 +0300

    some first code

--- Team4/src/main/java/com/example/Team4/Controllers/InsuredController.java
+++ Team4/src/main/java/com/example/Team4/Controllers/InsuredController.java
@@ -1,12 +1,14 @@
 package com.example.Team4.Controllers;
 
 import com.example.Team4.Models.Timeslot;
+import com.example.Team4.Models.VaccinationCenter;
 import com.example.Team4.Services.InsuredService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/insured")
+//mipws na valoume path variable edw??
 public class InsuredController {
 
     @Autowired
@@ -16,9 +18,13 @@ public class InsuredController {
     public Timeslot insuredSearchTimeslot (@RequestParam int day,
                                            @RequestParam int month,
                                            @RequestParam int year){
-        return insuredService.searchTimeslot(day, month, year);
+        //το βαζω για να μην βγαζει error στην insuredservice
+        // αλλα δεν μου βγαζει νοημα να βρισκεται εδω να βρω κατι αλλο
+        //και να βγαλω το vaccenter απο την μεθοδο
+        VaccinationCenter vaccinationCenter = new VaccinationCenter("acode", "anaddress");
+        return insuredService.searchTimeslot(day, month, year, vaccinationCenter);
     }
-    @GetMapping("/selectTimeslot")
+    @PostMapping("/selectTimeslot")
     public Timeslot InsuredSelectTimeslot (@RequestParam Long insuredAmka,
                                            @RequestBody Timeslot timeslot,
                                            @RequestParam Long doctorAmka){
