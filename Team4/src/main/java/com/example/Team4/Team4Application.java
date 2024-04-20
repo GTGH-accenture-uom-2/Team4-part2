8b64f941783c67584cc19f708432078db17e3d9e
GiorgosSamourgkanidis <georsamourgka22@hotmail.com>
2024-04-20 19:13:06 +0300

    Create Project

---
+++ Team4/src/main/java/com/example/Team4/Team4Application.java
@@ -0,0 +1,14 @@
+package com.example.Team4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team4Application.class, args);
+		System.out.println("lalalal");
+	}
+
+}
