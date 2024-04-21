1251bc22da5f27bd6972a815f713d919c9cd610b
iliannamaniatea <ilianna.mani@gmail.com>
2024-04-21 16:42:35 +0300

    first changes

--- Team4/src/main/java/com/example/Team4/Services/DoctorService.java
+++ Team4/src/main/java/com/example/Team4/Services/DoctorService.java
@@ -1,7 +1,21 @@
 package com.example.Team4.Services;
 
+import com.example.Team4.Models.Doctor;
+import com.example.Team4.Models.Vaccination;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class DoctorService {
+
+    List<Doctor> doctors = new ArrayList<>();
+
+    public List<Doctor> addDoctor(Doctor doctor){
+        doctors.add(doctor);
+        return doctors;
+    }
+
+
 }
--- Team4/src/main/java/com/example/Team4/Services/InsuredService.java
+++ Team4/src/main/java/com/example/Team4/Services/InsuredService.java
@@ -1,14 +1,22 @@
 package com.example.Team4.Services;
 
-import com.example.Team4.Models.Reservation;
-import com.example.Team4.Models.Timeslot;
-import com.example.Team4.Models.VaccinationCenter;
+import com.example.Team4.Models.*;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class InsuredService {
+
+    List<Insured> insureds = new ArrayList<>();
+
+    public List<Insured> addInsured(Insured insured){
+        insureds.add(insured);
+        return insureds;
+    }
+
+
     public Timeslot searchTimeslot(int day, int month, int year, VaccinationCenter vaccinationCenter) {
         ArrayList<Timeslot> allTimeslots = vaccinationCenter.getTimeslots();
         //ektupwsi twn free
