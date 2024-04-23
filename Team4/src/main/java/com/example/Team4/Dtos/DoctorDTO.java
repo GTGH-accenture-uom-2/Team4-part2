844aea3ccb5b379a14b64ca1b4e383b2316942c2
GiorgosSamourgkanidis <georsamourgka22@hotmail.com>
2024-04-23 16:26:09 +0300

    Create DoctorDTO

---
+++ Team4/src/main/java/com/example/Team4/Dtos/DoctorDTO.java
@@ -0,0 +1,10 @@
+package com.example.Team4.Dtos;
+
+import com.example.Team4.Models.Person;
+
+public class DoctorDTO extends Person {
+
+    public DoctorDTO(String name, String surname, Long amka) {
+        super(name, surname, amka);
+    }
+}
