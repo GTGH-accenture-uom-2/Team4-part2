73d10d7ca0658fc8653e0226b073b54a7695d856
matinampoz <matinampoz@gmail.com>
2024-04-23 17:32:53 +0300

    extra edits

--- Team4/src/main/java/com/example/Team4/Services/ReservationService.java
+++ Team4/src/main/java/com/example/Team4/Services/ReservationService.java
@@ -32,6 +32,9 @@ public class ReservationService {
     @Autowired
     private InsuredService insuredService;
 
+    @Autowired
+    private VaccinationCenterService vaccinationCenterService;
+
 
     public List<Reservation> addReservation(Reservation reservation) {
         reservations.add(reservation);
